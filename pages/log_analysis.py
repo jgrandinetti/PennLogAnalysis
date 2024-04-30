e0b6e40fa6b1531a73cad9fcea9eabd86be822f2
Jace Grandinetti <40122443+jgrandinetti@users.noreply.github.com>
2024-04-30 19:11:20 -0400

    Update log_analysis.py

--- pages/log_analysis.py
+++ pages/log_analysis.py
@@ -1,9 +1,11 @@
 from pylinac import load_log
+from pylinac.log_analyzer import anonymize
 import streamlit as st
 import tempfile
 import shutil
 import matplotlib.pyplot as plt
 from io import BytesIO
+
 
 # Function to handle file upload; this function will be called only when the file is uploaded.
 def save_uploaded_file(uploaded_file):
@@ -20,26 +22,25 @@ if "uploaded_file" not in st.session_state and uploaded_file is not None:
     save_uploaded_file(uploaded_file)
 
 if "uploaded_file" in st.session_state:
-    # This means the file is already uploaded and is stored in session_state
-    # You can now read the file or process it as required
     file = st.session_state.uploaded_file
-    st.write("File uploaded successfully. File name:", file.name)
+    # st.write("File uploaded successfully. File name:", file.name)
 
     with tempfile.NamedTemporaryFile(delete=False, suffix=uploaded_file.name) as tmp_file:
-        # Copy the contents of the uploaded file to the temporary file
         shutil.copyfileobj(uploaded_file, tmp_file)
         tmp_file_path = tmp_file.name  # Store the temporary file path
-        
-    log = load_log(tmp_file_path)
+
+    # After saving the file to a temporary location, anonymize it before processing
+    anonymize(tmp_file_path)  # Anonymize the log file at the temp path
+
+    log = load_log(tmp_file_path)  # Now load the anonymized log file
     log.fluence.actual.calc_map()
-    # log.fluence.actual.plot_map()
 
     plt.figure()  # Creates a new figure
-    log.fluence.actual.plot_map()  # This plots to the current figure
-    buf = BytesIO()  # Create a buffer to hold the image data
-    plt.savefig(buf, format='png')  # Save the current figure into the buffer in PNG format
-    plt.close()  # Close the plt figure to free memory
-    buf.seek(0)  # Seek to the start of the buffer
-    
+    log.fluence.actual.plot_map()
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    plt.close()
+    buf.seek(0)
+
     # Display the plot
     st.image(buf, caption='Fluence Map')
