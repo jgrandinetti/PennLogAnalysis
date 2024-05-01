7132abeb57a78cdb4d0e6581ed8b7876bd5765cf
Jace Grandinetti <40122443+jgrandinetti@users.noreply.github.com>
2024-05-01 11:15:48 -0400

    Update log_analysis.py

--- pages/log_analysis.py
+++ pages/log_analysis.py
@@ -18,7 +18,7 @@ import math
 def plot_heatmaps_to_buffer(calculated_fluence, expected_fluence, gamma):
     fig = sp.make_subplots(rows=1, cols=3, subplot_titles=("Calculated Fluence", "Expected Fluence", "Gamma"))
     
-    trace1 = px.imshow(calculated_fluence, text_auto=True, aspect="square", color_continuous_scale='asdf')
+    trace1 = px.imshow(calculated_fluence, text_auto=True, aspect="square", color_continuous_scale='jet')
     fig.add_trace(trace1.data[0], row=1, col=1)
     
     # Plot expected fluence heatmap
