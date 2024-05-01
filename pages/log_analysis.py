2e787bc530f0497efa0b0736987f6a082e757d19
Jace Grandinetti <40122443+jgrandinetti@users.noreply.github.com>
2024-05-01 11:43:46 -0400

    Update log_analysis.py

--- pages/log_analysis.py
+++ pages/log_analysis.py
@@ -168,7 +168,7 @@ def log_info():
     if "log" in st.session_state:
         log = st.session_state.log
 
-        st.write("\n Log Information \n")
+        st.write("\n\n\n Log Information \n")
 
         samp_int = log.header.sampling_interval
         snaps = log.header.num_snapshots
