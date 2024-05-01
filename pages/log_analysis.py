848bd22a9f2f45ace70439eb174b0db31e9bd30d
Jace Grandinetti <40122443+jgrandinetti@users.noreply.github.com>
2024-04-30 23:38:02 -0400

    Update log_analysis.py

--- pages/log_analysis.py
+++ pages/log_analysis.py
@@ -47,7 +47,7 @@ from streamlit_echarts import st_echarts
 
 
 def plot_heatmaps_to_buffer(calculated_fluence, expected_fluence, gamma):
-    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5), constrained_layout=True)
 
     # Titles for each subplot
     titles = ['Calculated Fluence', 'Expected Fluence', 'Gamma']
@@ -56,11 +56,9 @@ def plot_heatmaps_to_buffer(calculated_fluence, expected_fluence, gamma):
     data = [calculated_fluence, expected_fluence, gamma]
 
     for ax, d, title in zip(axes, data, titles):
-        heatmap = ax.imshow(d, cmap='hot', interpolation='nearest')
+        heatmap = ax.imshow(d, cmap='hot', interpolation='nearest', aspect='auto')
         ax.set_title(title)
         fig.colorbar(heatmap, ax=ax)
-
-    plt.tight_layout()
 
     # Save to buffer
     buf = BytesIO()
