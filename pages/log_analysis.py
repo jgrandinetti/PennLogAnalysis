e82e2bfd774ce6b1506fe3774794516025da9544
Jace Grandinetti <40122443+jgrandinetti@users.noreply.github.com>
2024-04-30 21:49:17 -0400

    Update log_analysis.py

--- pages/log_analysis.py
+++ pages/log_analysis.py
@@ -7,22 +7,7 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 import plotly.express as px
 import numpy as np
-import streamlit_echarts as st_echarts
 
-
-# ------- Functions --------- #
-def plot_heatmap():
-    option = {
-        "xAxis": {
-            "type": "category",
-            "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-        },
-        "yAxis": {"type": "value"},
-        "series": [{"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}],
-    }
-    st_echarts(
-        options=option, height="400px",
-    )
 
 
 def save_uploaded_file(uploaded_file):
