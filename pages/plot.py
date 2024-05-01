bfa0cc2b9c6857850dd7aa7d4c5e8ea677f1e8cd
Jace Grandinetti <40122443+jgrandinetti@users.noreply.github.com>
2024-05-01 01:40:47 -0400

    Update plot.py

--- pages/plot.py
+++ pages/plot.py
@@ -15,17 +15,5 @@ z = [[.1, .3, .5, .7, .9],
      [.9, .8, .4, .2, 0],
      [.3, .4, .5, .7, 1]]
 
-fig = px.imshow(z, text_auto=True)
-
-# Update layout to maintain equal aspect ratio
-fig.update_layout(
-    autosize=False,
-    width=500,  # Adjust width to your preference
-    height=500,  # Adjust height to match the width or according to your data aspect ratio
-    margin=dict(l=20, r=20, t=20, b=20),
-)
-
+fig = px.imshow(z, text_auto=True, aspect="square")
 st.plotly_chart(fig)
-
-
-
