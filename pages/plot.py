ca6d1862267b4cd7771c8db0426325674fd634f7
Jace Grandinetti <40122443+jgrandinetti@users.noreply.github.com>
2024-04-30 21:52:10 -0400

    Update plot.py

--- pages/plot.py
+++ pages/plot.py
@@ -1,13 +1,47 @@
 from streamlit_echarts import st_echarts
 
+# options = {
+#     "xAxis": {
+#         "type": "category",
+#         "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+#     },
+#     "yAxis": {"type": "value"},
+#     "series": [
+#         {"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}
+#     ],
+# }
+# st_echarts(options=options)
+
+
 options = {
-    "xAxis": {
-        "type": "category",
-        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    },
-    "yAxis": {"type": "value"},
+    "tooltip": {"trigger": "item"},
+    "legend": {"top": "5%", "left": "center"},
     "series": [
-        {"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}
+        {
+            "name": "访问来源",
+            "type": "pie",
+            "radius": ["40%", "70%"],
+            "avoidLabelOverlap": False,
+            "itemStyle": {
+                "borderRadius": 10,
+                "borderColor": "#fff",
+                "borderWidth": 2,
+            },
+            "label": {"show": False, "position": "center"},
+            "emphasis": {
+                "label": {"show": True, "fontSize": "40", "fontWeight": "bold"}
+            },
+            "labelLine": {"show": False},
+            "data": [
+                {"value": 1048, "name": "搜索引擎"},
+                {"value": 735, "name": "直接访问"},
+                {"value": 580, "name": "邮件营销"},
+                {"value": 484, "name": "联盟广告"},
+                {"value": 300, "name": "视频广告"},
+            ],
+        }
     ],
 }
-st_echarts(options=options)
+st_echarts(
+    options=options, height="500px",
+)
