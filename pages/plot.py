b1ba831d1193a8eb186d250848f8b88401211de2
Jace Grandinetti <40122443+jgrandinetti@users.noreply.github.com>
2024-05-01 01:38:27 -0400

    Update plot.py

--- pages/plot.py
+++ pages/plot.py
@@ -16,5 +16,13 @@ z = [[.1, .3, .5, .7, .9],
      [.3, .4, .5, .7, 1]]
 
 fig = px.imshow(z, text_auto=True)
+
+# Set the aspect ratio to be equal
+fig.update_layout(
+    xaxis=dict(scaleanchor="y", scaleratio=1),
+    yaxis=dict(constrain='domain')  # This ensures that the scale ratio is maintained
+)
+
 st.plotly_chart(fig)
+
 
